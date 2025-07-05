93b571804db22e734bfc246d27c02d03bb46daa9
yukishirotsubasa <bruce49721040@hotmail.com>
2025-07-05 20:30:45 +0800

    增加回收公式例外清單

--- js/recycleCost.js
+++ js/recycleCost.js
@@ -12,12 +12,23 @@ import { createItemNameMap, getItemSellPrice, getMaterialPrice, formatNumberWith
 export function generateRecycleCostTableData(FORGE_FORMULAS, generateTableHTML, createItemNameMap, itemBase) {
     // 獲取 imageSheet 數據
     const imageSheet = window.allData?.imageSheet || null;
+    const exception_list = [186,187,188,189,190,191,192,193,194,195,196,197,55,73,74,58,75,76,89,90,91,94,95,96,124,125,126,
+        120,121,122,116,117,118,155,156,157,151,152,153,159,160,161,202,203,204,206,207,208,210,211,212,214,215,216,
+        276,277,278,280,281,282,284,285,286,288,289,290,292,293,294,296,297,298,300,301,302,304,305,306,308,309,310,
+        312,313,314,316,317,318,320,321,322,324,325,1061,327,328,329,331,332,333,335,336,337,339,340,341,343,344,345,
+        347,348,349,351,352,353,358,359,360,362,363,364,366,367,368,370,371,372,375,376,377,379,380,381,383,384,385,
+        387,388,389,393,394,395,397,398,399,401,402,403,405,406,407,410,411,412,414,415,416,418,419,420,422,423,424,
+        438,558,559,560,562,563,564,566,567,568,570,571,572,574,575,576,578,579,580,582,583,584,586,587,588,590,591,592,
+        594,595,596,598,599,600,602,603,604,611,612,613,650,719,20,609,440,539,540,541,542,543,765,766,767,768,769,770,
+        771,772,773,774,775,776
+    ];
+    //exception_list.includes(Number(id)) || 
 
     return Object.keys(FORGE_FORMULAS).flatMap(id => {
         const formula = FORGE_FORMULAS[id];
         
         // 1. only_smelt為true的不可分解，可直接去除
-        if (formula.only_smelt || (formula.chance == 1 && !formula.recycle_chance)) {
+        if (formula.only_smelt || itemBase[formula.item_id].params.no_smelt) {
             return [];
         }
 
