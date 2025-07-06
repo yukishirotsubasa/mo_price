4f2748e0b3b26d26d332c3e05eb2c09de8d704e1
yukishirotsubasa <bruce49721040@hotmail.com>
2025-07-06 15:12:21 +0800

    更新翻譯

--- js/controllers/UIController.js
+++ js/controllers/UIController.js
@@ -45,6 +45,7 @@ export class UIController {
             presentTabButton: '.tab-button[data-tab="present-page"]',
             breedingTabButton: '.tab-button[data-tab="breeding-page"]',
             recycleTabButton: '.tab-button[data-tab="recycle-page"]',
+            firelordSetTabButton: '.tab-button[data-tab="firelord-set-page"]',
 
             // 內容標題元素
             tab1ContentH2: '#tab1-content h2',
@@ -64,7 +65,8 @@ export class UIController {
             rareKeyContentH2: '#rare-key-page-content h2',
             presentContentH2: '#present-page-content h2',
             breedingContentH2: '#breeding-page-content h2',
-            recycleContentH2: '#recycle-page-content h2'
+            recycleContentH2: '#recycle-page-content h2',
+            firelordSetContentH2: '#firelord-set-page-content h2'
         };
 
         // 快取所有元素
@@ -123,6 +125,7 @@ export class UIController {
         this.updateTabButton('presentTabButton', 'Present');
         this.updateTabButton('breedingTabButton', 'Breeding');
         this.updateTabButton('recycleTabButton', 'Recycle');
+        this.updateTabButton('firelordSetTabButton', 'Firelord Set');
 
         // 更新Tab內容標題
         this.updateContentTitle('tab1ContentH2', 'Item');
@@ -143,6 +146,7 @@ export class UIController {
         this.updateContentTitle('presentContentH2', 'Present');
         this.updateContentTitle('breedingContentH2', 'Breeding Cost Calculator');
         this.updateContentTitle('recycleContentH2', 'Recycle Cost Calculator');
+        this.updateContentTitle('firelordSetContentH2', 'Firelord Set');
     }
 
     /**
